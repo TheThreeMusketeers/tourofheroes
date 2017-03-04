56273c7fd29d5a8205fc6a99069bdd9696a8a355
ersinsivaz <ersinsivaz@hotmail.com>
2017-03-04 19:05:48 +0300

    HTTP Getting and saving data

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -6,6 +7,7 @@ import { HeroService } from './hero.service';
 
 
 @Component({
+  moduleId:'module.id',
   selector: 'my-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
@@ -15,7 +17,7 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
   heroes: Hero[];
 
-  constructor(private heroService:HeroService){
+  constructor(private heroService:HeroService,private router:Router){
 
   }
 
@@ -31,7 +33,9 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroesSlowly().then(heroes=>this.heroes=heroes);
   }
 
-
+  gotoDetail(){
+    this.router.navigate(['/detail',this.selectedHero.id]);
+  }
 
 }
 
